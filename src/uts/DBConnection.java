9f81f6b06acef4451565f57493047283a14c3a4a
erma456 <ermarohmawati21@gmail.com>
2017-03-18 05:50:13 +0700

    versi 1.0.0

---
+++ src/uts/DBConnection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Erma
+ */
+class DBConnection {
+
+    static ResultSet executeQuery(String SQL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
